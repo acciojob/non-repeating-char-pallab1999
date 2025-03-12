bc1b0962d44dea66c258e00b92280972dc819594
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-12 21:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	
-	for(int i=0;i<str.length;i++){
+	for(let i=0;i<str.length;i++){
 		let f = false;
-		for(int j=0;j<str.length;j++){
+		for(let j=0;j<str.length;j++){
 			if(i!=j&&str[i]===str[j]){
 				f = true;
 				break;
