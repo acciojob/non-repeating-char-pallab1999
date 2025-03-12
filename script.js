fca64df851441f7e6b0b8899a9460721b64680bb
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-12 21:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	boolean f = false;
+	
 	for(int i=0;i<str.length;i++){
-		f = false;
+		let f = false;
 		for(int j=0;j<str.length;j++){
 			if(i!=j&&str[i]===str[j]){
 				f = true;
+				break;
 			}
 		}
-		if(f==false)return str[i];
+		if(f===false)return str[i];
 	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
